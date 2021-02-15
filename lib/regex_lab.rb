e20ec4d42d6b887d6eb6ae0e8d0b3bbb9b2dfa79
Sindhura <sindhura.gollamudi@yahoo.com>
2021-02-15 16:48:51 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,29 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/^[AEIOU]|^[aeiou]/)
+     true
+  else 
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
+    return_array = text.split(" ")
+    return_array.grep(/\b\w{5}\b/)
+
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   first_word = text.scan(/\w+/)[0]
+    if (text.scan(/\./))&& (first_word==first_word.upcase)
+      true
+    else
+      false
+    end
 end
 
 def valid_phone_number?(phone)
